fe99c7bd1bb03862345667a6e6beb5b2c93d7f79
Yaqiao <yyang169@csu.fullerton.edu>
2024-11-01 03:10:52 -0400

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,5 +26,4 @@
 </style>
 <br/><a href="/brucetest1">Check out the new page</a>
 <br/><a href="/brucetest2">Check out the new page</a>
-<br/><a href="/brucetest2">Why is not working?????????</a>
-<br/><a href="/brucetest2">Why is not working?????????</a>
+
